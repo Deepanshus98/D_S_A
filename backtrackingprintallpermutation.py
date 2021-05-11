e7211c9b310926f2999e8b6bbba58bc57ee98328
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-11 14:34:44 +0530

    Update backtrackingprintallpermutation.py

--- backtrackingprintallpermutation.py
+++ backtrackingprintallpermutation.py
@@ -1,25 +1,18 @@
-# Python program to print all permutations with 
-# duplicates allowed 
- 
-def toString(List): 
-    return ''.join(List) 
- 
-# Function to print permutations of string 
-# This function takes three parameters: 
-# 1. String 
-# 2. Starting index of the string 
-# 3. Ending index of the string. 
-def permute(a, l, r): 
-    if l==r: 
-        print toString(a) 
-    else: 
-        for i in xrange(l,r+1): 
-            a[l], a[i] = a[i], a[l] 
-            permute(a, l+1, r) 
-            a[l], a[i] = a[i], a[l] # backtrack 
- 
-# Driver program to test the above function 
-string = "ABC"
-n = len(string) 
-a = list(string) 
-permute(a, 0, n-1) 
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        if len(nums) == 0:
+        	return []
+        if len(nums) == 1:
+        	return [nums]
+
+        result = []
+        for index in range(len(nums)):
+        	for p in self.permute(nums[0:index] + nums[index+1:]):
+        		result.append([nums[index]] + p)
+
+        return result
