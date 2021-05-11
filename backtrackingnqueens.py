62b1a9568bf4f3d4d99d8c5578c6fec849300909
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-11 14:46:15 +0530

    Update backtrackingnqueens.py

--- backtrackingnqueens.py
+++ backtrackingnqueens.py
@@ -1,39 +1,38 @@
-from typing import LIST
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        def could_place(row, col):
-            return not (cols[col] + hill_diagonals[row - col] + dale_diagonals[row + col])
-        
-        def place_queen(row, col):
-            queens.add((row, col))
-            cols[col] = 1
-            hill_diagonals[row - col] = 1
-            dale_diagonals[row + col] = 1
-        
-        def remove_queen(row, col):
-            queens.remove((row, col))
-            cols[col] = 0
-            hill_diagonals[row - col] = 0
-            dale_diagonals[row + col] = 0
-        
-        def add_solution():
-            solution = []
-            for _, col in sorted(queens):
-                solution.append('.' * col + 'Q' + '.' * (n - col - 1))
-            output.append(solution)
-        
-        def backtrack(row = 0):
-            for col in range(n):
-                if could_place(row, col):
-                    place_queen(row, col)
-                    if row + 1 == n:
-                        add_solution()
-                    else:
-                        backtrack(row + 1)
-                    remove_queen(row, col)
-        
-        cols = [0] * n
-        hill_diagonals = [0] * (2 * n - 1)
-        dale_diagonals = [0] * (2 * n - 1)
-        queens = set()
-        output = []
+    def solveNQueens(self, n):
+        res = set()		# Storing the coordinates of the queen placed on each line
+        # 
+        def find_pos(row_index, queen, n):
+            for j in range(n):
+                if pos_available(queen, (row_index, j)):
+                    queen[row_index] = j
+                    if row_index == n-1:    # Already the last line and found the location where you can put it
+                        res.add(tuple(queen))
+                        return
+                    else:   # 
+                        find_pos(row_index+1, queen, n)
+		# Verify that the current location is available
+        def pos_available(queen, pos):
+            #   According to 0~i-1 line
+            for i in range(pos[0]):
+                if queen[i] == pos[1]:  # 
+                    return False
+                elif abs(i-pos[0]) == abs(queen[i]-pos[1]): #   diagonal
+                    return False
+            return True
+		# Print the coordinate form as Q...form
+        def plot_queen(queen, n):
+            out = []
+            for i in range(n):
+                s = '.' * queen[i] + 'Q' * 1 + '.' * (n-queen[i]-1)
+                out.append(s)
+            return out
+
+        # -1 Actually has no effect
+        queen = [-1 for _ in range(n)]  # initial position
+        # Recursion Starting from line 0
+        find_pos(0, queen, n)
+        out = []
+        for t in res:
+            out.append(plot_queen(t, n))
+        return out
