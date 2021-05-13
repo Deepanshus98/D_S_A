487f3ef4b3648dcbe2bf782742c9f7b661c47375
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-13 11:15:52 +0530

    Update bitcounttotalsetbitsbit.py

--- bitcounttotalsetbitsbit.py
+++ bitcounttotalsetbitsbit.py
@@ -1,13 +1,9 @@
-def getSetBitsFromOneToN(N):
-    two = 2
-    ans = 0
-    n = N
-    while(n != 0)
-    {
-        ans += int(N / two) * (two >> 1)
-        if((N & (two - 1)) > (two >> 1) - 1):
-            ans += (N&(two - 1)) - (two >> 1) + 1
-        two <<= 1; 
-        n >>= 1;
-    }
-    return ans
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        solution=[0]
+        for i in range (1,num+1):
+            if(i%2==0):
+                solution.append(solution[i//2])
+            else:
+                solution.append(solution[i-1]+1)
+        return solution
