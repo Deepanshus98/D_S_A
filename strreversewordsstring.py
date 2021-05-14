4a47dc64620ccc6ebd52e4fa63d2d5fa76deccff
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-14 21:36:13 +0530

    Update strreversewordsstring.py

--- strreversewordsstring.py
+++ strreversewordsstring.py
@@ -1,46 +1,11 @@
-# Python3 program to reverse a string
- 
-# Function to reverse each word in the string
-def reverse_word(s, start, end):
-    while start < end:
-        s[start], s[end] = s[end], s[start]
-        start = start + 1
-        end -= 1
- 
- 
-s = "i like this program very much"
- 
-# Convert string to list to use it as a char array
-s = list(s)
-start = 0
-while True:
-     
-    # We use a try catch block because for
-    # the last word the list.index() function
-    # returns a ValueError as it cannot find 
-    # a space in the list
-    try:
-        # Find the next space
-        end = s.index(' ', start)
- 
-        # Call reverse_word function
-        # to reverse each word
-        reverse_word(s, start, end - 1)
- 
-        #Update start variable
-        start = end + 1
- 
-    except ValueError:
- 
-        # Reverse the last word
-        reverse_word(s, start, len(s) - 1)
-        break
- 
-# Reverse the entire list
-s.reverse()
- 
-# Convert the list back to
-# string using string.join() function
-s = "".join(s)
- 
-print(s)
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        w_list = []
+        for eve_w in s.split(" "):
+            if eve_w:
+                w_list.append(eve_w)
+        return " ".join(reversed(w_list))
