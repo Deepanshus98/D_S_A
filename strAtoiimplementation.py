a7a891aa0f37a92cea55af60cd77ec1a9033fd40
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-14 21:56:36 +0530

    Update strAtoiimplementation.py

--- strAtoiimplementation.py
+++ strAtoiimplementation.py
@@ -19,3 +19,23 @@ string = "89789"
 
 # Function call
 print myAtoi(string)
+
+
+
+def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if haystack=='' and needle!='':return -1
+        if needle=='':return 0
+        
+        for i in range (len(haystack)-len(needle)+1):
+            if haystack[i]==needle[0]:
+                j=0
+                while j<len(needle):
+                    if haystack[i+j]!=needle[j]:break
+                    else:j+=1
+                if j==len(needle):return i
+        return -1
