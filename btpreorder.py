c4fcfe36c7e16e2dae43a5bc7180975fb9547003
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-15 23:22:42 +0530

    Update btpreorder.py

--- btpreorder.py
+++ btpreorder.py
@@ -1,47 +1,37 @@
-# Recursive function to perform preorder traversal on the tree
-def preorder(root):
- 
-    # return if the current node is empty
-    if root is None:
-        return
- 
-    # Display the data part of the root (or current node)
-    print(root.data, end=' ')
- 
-    # Traverse the left subtree
-    preorder(root.left)
- 
-    # Traverse the right subtree
-    preorder(root.right)
-
-
-
-# Iterative function to perform preorder traversal on the tree
-def preorderIterative(root):
- 
-    # return if the tree is empty
-    if root is None:
-        return
- 
-    # create an empty stack and push the root node
-    stack = deque()
-    stack.append(root)
- 
-    # loop till stack is empty
-    while stack:
- 
-        # pop a node from the stack and print it
-        curr = stack.pop()
- 
-        print(curr.data, end=' ')
- 
-        # push the right child of the popped node into the stack
-        if curr.right:
-            stack.append(curr.right)
- 
-        # push the left child of the popped node into the stack
-        if curr.left:
-            stack.append(curr.left)
- 
-    # the right child must be pushed first so that the left child
-    # is processed first (FIFO order
+#non recursion
+class Solution:
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        result=[]
+        stack=[]
+        stack.append(root)
+        while stack:
+            tmp=stack.pop()
+            result.append(tmp.val)
+            if tmp.right:
+                stack.append(tmp.right)
+            if tmp.left:
+                stack.append(tmp.left)
+        return result;
+#with recursion
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        #recursive writing
+        result=[]
+        def pre(root,result):
+            if root==None:
+                return None
+            result.append(root.val)
+            pre(root.left,result)
+            pre(root.right,result)
+        pre(root,result)
+        return result
