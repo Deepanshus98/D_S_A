9547406ea99cb0fc09fd6ba97bfab1944e0b171b
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-16 11:51:14 +0530

    Update btlevalspiralsearchbt.py

--- btlevalspiralsearchbt.py
+++ btlevalspiralsearchbt.py
@@ -1,20 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        res = self.bfs(root)
-        res = [res[i] if i%2 == 0 else res[i][::-1] for i in range(len(res))]
-        return res
-        
-    def bfs(self, root):
-        res = []
-        if not root:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+            if root is None:
+                return []
+            res = []
+            stack = [root]
+            left = True # True: left->right,  False: right->left
+            while stack:
+                tmp = []
+                tmp_stack=[]
+                for _ in range(len(stack)):
+                    node = stack.pop()
+                    tmp.append(node.val)
+                    if left:
+                        if node.left: tmp_stack.append(node.left)
+                        if node.right: tmp_stack.append(node.right)
+                    else:
+                        if node.right: tmp_stack.append(node.right)
+                        if node.left: tmp_stack.append(node.left)
+                res.append(tmp)
+                stack = tmp_stack
+                left = not left
             return res
-        q = [root]
-        while q:
-            res.append([node.val for node in q])
-            q = [kid for node in q for kid in (node.left, node.right) if kid]
-            
-        return res
