2c3ecdfdf3ca63ee2d940842bd01f9f285d8d998
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-16 11:15:38 +0530

    Update btPOSTORDERBT.py

--- btPOSTORDERBT.py
+++ btPOSTORDERBT.py
@@ -10,27 +10,14 @@ class Solution:
         return res
 #without recursion
 
-class Solution:  
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        result = list()  #Storing the final result
-        if root == None:
-            return result
-        
-        stack = list()  # 
-        while stack or root:
-            if root != None:   
-                stack.append(root)
-                root = root.right
-            else:
-                root = stack.pop()
-                result.append(root.val)
-                root = root.right
-               
-                root = stack.pop()
-                stack.append(root)
-
-        return result
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        def helper(root, l):
+            if root is None:
+                return
+            helper(root.left, l)
+            helper(root.right, l)
+            l.append(root.val)
+        l = []
+        helper(root, l)
+        return l
