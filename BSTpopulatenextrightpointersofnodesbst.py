f5e6812b9fb8d4970a5ccc2c180ae6ee5d973dff
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-17 15:14:57 +0530

    Update BSTpopulatenextrightpointersofnodesbst.py

--- BSTpopulatenextrightpointersofnodesbst.py
+++ BSTpopulatenextrightpointersofnodesbst.py
@@ -1,30 +1,10 @@
-
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, left, right, next):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if root == None:
+        if root is None or root.left is None:
             return root
-        queue = []
-        queue.append(root)
-        while queue:
-            curtmp = []
-            nextmp = []
-            for node in queue:
-                curtmp.append(node)
-                if node.left:
-                    nextmp.append(node.left)
-                if node.right:
-                    nextmp.append(node.right)
-            queue = nextmp
-            for i in range(len(curtmp)-1):
-                curtmp[i].next = curtmp[i+1]
-            # curtmp[-1].next = None   no necessory
+        root.left.next = root.right
+        if root.next:
+            root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
         return root
