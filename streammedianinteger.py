63db4ff0cc63c971e5173cfd98da4bb42a116ab7
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-17 23:00:58 +0530

    Update streammedianinteger.py

--- streammedianinteger.py
+++ streammedianinteger.py
@@ -1,50 +1,46 @@
-'''
-	Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
-	Examples: 
-	[2,3,4] , the median is 3
-	[2,3], the median is (2 + 3) / 2 = 2.5
-	Design a data structure that supports the following two operations:
-	void addNum(int num) - Add a integer number from the data stream to the data structure.
-	double findMedian() - Return the median of all elements so far.
-	For example:
-	addNum(1)
-	addNum(2)
-	findMedian() -> 1.5
-	addNum(3) 
-	findMedian() -> 2
-'''
-import heapq
-class MedianFinder(object):
+from heapq import heappush, heappop, heappushpop
 
+class MedianFinder:
     def __init__(self):
         """
-        initialize your data structure here.
+        Initialize your data structure here.
         """
-        self.max_heap = []
-        self.min_heap = []
-        
+        self.upperHeap = [float('inf')]
+        self.lowerHeap = [float('inf')]
+        # lowerHeap's numbers are minus original numbers, because in Python heap is min-heap
+
+        # always maintain that their lens are equal, or upper has 1 more than lower
 
     def addNum(self, num):
         """
+        Adds a num into the data structure.
         :type num: int
         :rtype: void
         """
-        if not self.max_heap or num > -self.max_heap[0]:
-        	heapq.heappush(self.min_heap, num)
+        upperMin = + self.upperHeap[0]
+        lowerMax = - self.lowerHeap[0]
 
-        	if len(self.min_heap) > len(self.max_heap) + 1:
-        		heapq.heappush(self.max_heap, -heapq.heapop(self.min_heap))
-		else:
-			heapq.heappush(self.max_heap, -num)
-			if len(self.max_heap) > len(self.min_heap):
-				heapq.heappush(self.min_heap, -heapq.heapop(self.max_heap))        
+        if num > upperMin or (lowerMax<=num<=upperMin and len(self.upperHeap)==len(self.lowerHeap)):
+            heappush(self.upperHeap, num)
+        else:
+            heappush(self.lowerHeap, -num)
+
+        # maintain the invariant that their lens are equal, or upper has 1 more than lower
+        if len(self.upperHeap)-len(self.lowerHeap) > 1:
+            heappush( self.lowerHeap, -heappop( self.upperHeap ) )
+        elif len(self.lowerHeap) > len(self.upperHeap):
+            heappush( self.upperHeap, -heappop( self.lowerHeap ) )
+
 
     def findMedian(self):
         """
+        Returns the median of current data stream
         :rtype: float
         """
-        print self.max_heap, self.min_heap
-        if len(self.max_heap) == len(self.min_heap):
-        	return (-self.max_heap[0]+self.min_heap[0] )/2.0
+        if len(self.upperHeap) == len(self.lowerHeap):
+            upperMin = + self.upperHeap[0]
+            lowerMax = - self.lowerHeap[0]
+            return ( float(upperMin) + float(lowerMax) ) / 2.0
         else:
-        	return self.min_heap[0]
+            assert len(self.upperHeap) == len(self.lowerHeap) + 1
+            return float(self.upperHeap[0])
