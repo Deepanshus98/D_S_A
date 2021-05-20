d8a389111a63700ad59c2c32936460cd84d06d4e
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-05-20 18:39:44 +0530

    Update dpmatrixchainmultiplication.py

--- dpmatrixchainmultiplication.py
+++ dpmatrixchainmultiplication.py
@@ -1,47 +1,53 @@
-# Dynamic Programming Python implementation of Matrix 
-# Chain Multiplication. See the Cormen book for details 
-# of the following algorithm 
-import sys 
-  
-# Matrix Ai has dimension p[i-1] x p[i] for i = 1..n 
-  
-  
-def MatrixChainOrder(p, n): 
-    # For simplicity of the program,  
-    # one extra row and one 
-    # extra column are allocated in m[][].   
-    # 0th row and 0th 
-    # column of m[][] are not used 
-    m = [[0 for x in range(n)] for x in range(n)] 
-  
-    # m[i, j] = Minimum number of scalar  
-    # multiplications needed 
-    # to compute the matrix A[i]A[i + 1]...A[j] =  
-    # A[i..j] where 
-    # dimension of A[i] is p[i-1] x p[i] 
-  
-    # cost is zero when multiplying one matrix. 
-    for i in range(1, n): 
-        m[i][i] = 0
-  
-    # L is chain length. 
-    for L in range(2, n): 
-        for i in range(1, n-L + 1): 
-            j = i + L-1
-            m[i][j] = sys.maxint 
-            for k in range(i, j): 
-  
-                # q = cost / scalar multiplications 
-                q = m[i][k] + m[k + 1][j] + p[i-1]*p[k]*p[j] 
-                if q < m[i][j]: 
-                    m[i][j] = q 
-  
-    return m[1][n-1] 
-  
-  
-# Driver code 
-arr = [1, 2, 3, 4] 
-size = len(arr) 
-  
-print("Minimum number of multiplications is " +
-      str(MatrixChainOrder(arr, size)))
+import sys
+ 
+ 
+# Function to find the most efficient way to multiply
+# a given sequence of matrices
+def matrixChainMultiplication(dims, i, j):
+ 
+    # base case: one matrix
+    if j <= i + 1:
+        return 0
+ 
+    # stores the minimum number of scalar multiplications (i.e., cost)
+    # needed to compute matrix `M[i+1] … M[j] = M[i…j]`
+    min = sys.maxsize
+ 
+    # take the minimum over each possible position at which the
+    # sequence of matrices can be split
+ 
+    '''
+        (M[i+1]) × (M[i+2]………………M[j])
+        (M[i+1]M[i+2]) × (M[i+3…………M[j])
+        …
+        …
+        (M[i+1]M[i+2]…………M[j-1]) × (M[j])
+    '''
+ 
+    for k in range(i + 1, j):
+ 
+        # recur for `M[i+1]…M[k]` to get an `i × k` matrix
+        cost = matrixChainMultiplication(dims, i, k)
+ 
+        # recur for `M[k+1]…M[j]` to get an `k × j` matrix
+        cost += matrixChainMultiplication(dims, k, j)
+ 
+        # cost to multiply two `i × k` and `k × j` matrix
+        cost += dims[i] * dims[k] * dims[j]
+ 
+        if cost < min:
+            min = cost
+ 
+    # return the minimum cost to multiply `M[j+1]…M[j]`
+    return min
+ 
+ 
+# Matrix Chain Multiplication Problem
+if __name__ == '__main__':
+ 
+    # Matrix `M[i]` has dimension `dims[i-1] × dims[i]` for `i = 1…n`
+    # input is `10 × 30` matrix, `30 × 5` matrix, `5 × 60` matrix
+    dims = [10, 30, 5, 60]
+ 
+    print("The minimum cost is", matrixChainMultiplication(dims, 0, len(dims) - 1))
+ 
